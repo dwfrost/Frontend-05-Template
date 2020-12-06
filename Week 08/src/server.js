cb6fb3592d1e3c2d49661dd90da856e982ac67d3
dwfrost <dwfrost@163.com>
2020-12-06 16:23:12 +0800

    toy broswer

--- Week 08/src/server.js
+++ Week 08/src/server.js
@@ -1,5 +1,4 @@
 const http = require('http')
-console.log(http)
 
 const server = http.createServer((request, response) => {
   let body = []
@@ -11,14 +10,15 @@ const server = http.createServer((request, response) => {
       body.push(chunk.toString())
     })
     .on('end', () => {
-      body = Buffer.concat(body).toString()
+      // body = Buffer.concat(body).toString()
+      body = body.join('')
 
       console.log('body:', body)
       response.writeHead(200, { 'Content-Type': 'text/html' })
-      response.end('Hello World \n')
+      response.end(' Hello World \n')
     })
 })
 
-server.listen(8080)
+server.listen(8088)
 
 console.log('server is listening')
