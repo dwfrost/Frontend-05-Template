674ab234e2dd4051d705de499e3e2a2faf3d1302
dwfrost <dwfrost@163.com>
2021-01-06 09:17:37 +0800

    createElement

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -1,5 +1,30 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i)
+function createElement(tag, attributes, ...children) {
+  const dom = document.createElement(tag)
+
+  for (let key in attributes) {
+    dom.setAttribute(key, attributes[key])
+  }
+
+  for (let child of children) {
+    dom.appendChild(child)
+  }
+  return dom
 }
 
-const mydiv = <div className="myclass">123</div>
+const mydiv = (
+  <div className="myclass">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+)
+
+console.log('mydiv', mydiv)
+document.body.appendChild(mydiv)
+
+// var mydiv = createElement("div", {
+//   className: "myclass"
+// },
+// createElement("span", null, "1"),
+// createElement("span", null, "2"),
+// createElement("span", null, "3"));
