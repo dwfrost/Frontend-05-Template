675653e5e983a74314e50cec6f5cbbcab5cbb6ee
dwfrost <dwfrost@163.com>
2021-02-17 14:12:32 +0800

    eslint

---
+++ Week 20/eslint-demo/.eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {},
+}
---
+++ Week 20/eslint-demo/index.js
@@ -0,0 +1,6 @@
+let a = 1
+
+console.log(a)
+for (let i of [1, 2, 3]) {
+  console.log(i)
+}
